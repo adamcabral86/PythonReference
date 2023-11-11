ae0a35ab844c5f25094cd5516c6adc59880864be
adamcabral86 <adamcabral86@hotmail.com>
2023-11-11 13:48:51 -0700

    Formatted the index.txt

--- working.py
+++ working.py
@@ -1,2 +1,19 @@
-with open("index.txt") as file:
-    print(file.read())
+with open("index.txt", "r", encoding="utf-8") as file:
+    with open("index.txt", "a", encoding="utf-8") as fileAppend:
+
+        for line in file.readlines():
+            newStr = ""
+            if line == "\n":
+                continue
+            else:
+                for word in line.split(" "):
+                    if word == line.split(" ")[1] or word == line.split(" ")[-1]:
+                        continue
+                    else:
+                        newStr += word +' '
+            newStr = newStr.strip()
+            #for word in line.split(" "):
+                #print(line.split(" "), end="")
+            fileAppend.write((newStr.title()) + "\n")
+            print(newStr.title())
+        #print(file.read())
